4f35eec3824bc8de67dff8960d39f7b93636007a
zakariabounou <zakariabounouu@gmail.com>
2022-12-23 00:09:08 +0100

    Insertion des entites des utilisateurs

---
+++ src/main/java/com/gestion/models/adminEntity.java
@@ -0,0 +1,18 @@
+package com.gestion.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Admin")
+
+public class adminEntity extends utilisateurEntity{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
--- src/main/java/com/gestion/models/utilisateurEntity.java
+++ src/main/java/com/gestion/models/utilisateurEntity.java
@@ -6,17 +6,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "utilisateur")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class utilisateurEntity implements Serializable{
+public abstract class utilisateurEntity implements Serializable{
 	
 	/**
 	 * 
